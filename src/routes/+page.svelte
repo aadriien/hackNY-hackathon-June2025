16a4ac7acafd731229809c934a89c0cb4793d90c
aadriien <adriienlynch@gmail.com>
2025-06-07 12:51:57 -0400

    Installed face-api.js & pulled model weights

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Face from '$lib/components/face.svelte';
+    import Questionnaires from '$lib/components/Questionnaires.svelte';
+    import ResultDisplay from '$lib/components/ResultDisplay.svelte';
+</script>
+
+
+<main></main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+    
+
+<main></main>
