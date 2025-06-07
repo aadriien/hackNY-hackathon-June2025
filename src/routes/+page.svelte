3740c0eab8421647fd32c3d46ff793527925d97e
aadriien <adriienlynch@gmail.com>
2025-06-07 13:55:57 -0400

    Began outlining face detection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,40 @@
 <script>
+    import { onMount } from "svelte";
     import Face from '$lib/components/face.svelte';
     import Questionnaires from '$lib/components/Questionnaires.svelte';
     import ResultDisplay from '$lib/components/ResultDisplay.svelte';
+
+
+    let detections = {};
+    let expressionHistory;
+
+    
+    async function waitForExpressionHistory() {
+        while (!expressionHistory) {
+            await new Promise(r => setTimeout(r, 50));
+        }
+    }
+
+    onMount(async () => {
+        await waitForExpressionHistory();
+
+        expressionHistory.startContinuousExpressionTracking();
+        const emotionSummary = await expressionHistory.collectExpressions(5000);
+        console.log(emotionSummary);
+    });
+
+
+
 </script>
 
 
-<main></main>
+<main>
     <h1>Welcome to SvelteKit</h1>
     <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-    
+    <div class="face-row">
+        <Face bind:detections bind:this={expressionHistory} />
+    </div>
 
-<main></main>
+
+</main>
