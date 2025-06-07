1ce7fb14bfc2c4935f9a5334cc5ec4c66017e441
Dustin Tran <dustintr@udel.edu>
2025-06-07 13:01:47 -0400

    Header & global font up

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,26 +1,28 @@
-<!-- Header.svelte -->
-<header class="header">
-  <img src="/assets/VibeCheckerLogo.svg" alt="Logo" class="logo" />
-  <h1 class="title">Wanna know if you pass the vibe check for NYC restaurants and bars? You probably don't anyways so don't even bother to try</h1>
-</header>
+<!-- src/lib/components/Header.svelte -->
+<div class="header">
+  <img src="src/assets/VibeCheckerLogo.svg" alt="Logo" class="logo" />
+  <h1 class="title">I'm Vibe Reader --- Read that carefully now</h1>
+  <p>We're gonna check if you pass the vibe for nyc's restaurants and bars. You probably won't anyways, but it doesn't hurt to try </p>
+</div>
 
 <style>
   .header {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-    gap: 1rem;
-    padding: 1rem;
-    background-color: #f3f4f6;
+    margin-top: 3rem;
+    text-align: center;
   }
 
   .logo {
-    width: 48px;
-    height: 48px;
+    width: 250px;
+    height: auto;
+    margin-bottom: 1rem;
   }
 
   .title {
-    font-size: 1.25rem;
-    font-weight: bold;
+    font-size: 2.5rem;
+    font-weight: 600;
+    font-family: 'Playfair Display', serif;
   }
 </style>
